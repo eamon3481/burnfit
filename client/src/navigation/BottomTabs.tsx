f110036645918031eb1baab9844b8954d3eff59e
Dongmin Ahn <eamon3481@naver.com>
2022-10-22 00:04:15 +0900

    feat: add Ionicons

--- client/src/navigation/BottomTabs.tsx
+++ client/src/navigation/BottomTabs.tsx
@@ -5,16 +5,45 @@ import Calendar from '../screens/Calendar';
 import Home from '../screens/Home';
 import Library from '../screens/Library';
 import MyPage from '../screens/MyPage';
+import Ionicons from 'react-native-vector-icons/Ionicons';
 
 const Tab = createBottomTabNavigator();
 const TabNavigation: React.FC = () => {
   return (
     <NavigationContainer>
       <Tab.Navigator initialRouteName="HOME">
-        <Tab.Screen name="HOME" component={Home} />
-        <Tab.Screen name="CALENDAR" component={Calendar} />
-        <Tab.Screen name="LIBRARY" component={Library} />
-        <Tab.Screen name="MyPage" component={MyPage} />
+        <Tab.Screen
+          name="HOME"
+          component={Home}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => <Ionicons size={24} name="home" />,
+          }}
+        />
+        <Tab.Screen
+          name="CALENDAR"
+          component={Calendar}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => <Ionicons size={24} name="calendar" />,
+          }}
+        />
+        <Tab.Screen
+          name="LIBRARY"
+          component={Library}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => <Ionicons size={24} name="library" />,
+          }}
+        />
+        <Tab.Screen
+          name="MYPAGE"
+          component={MyPage}
+          options={{
+            headerShown: false,
+            tabBarIcon: () => <Ionicons size={24} name="people" />,
+          }}
+        />
       </Tab.Navigator>
     </NavigationContainer>
   );
