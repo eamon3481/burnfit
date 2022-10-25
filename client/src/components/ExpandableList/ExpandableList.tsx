d40e79c970ee7219f80b673b7bb3e4537137f0c9
Dongmin Ahn <eamon3481@naver.com>
2022-10-25 10:40:27 +0900

    feat: add PanGestureHandler and translateY

--- client/src/components/ExpandableList/ExpandableList.tsx
+++ client/src/components/ExpandableList/ExpandableList.tsx
@@ -1,18 +1,27 @@
-import { View } from 'react-native';
+import { Button, View } from 'react-native';
 import React, { useState } from 'react';
-import Animated from 'react-native-reanimated';
+import Animated, {
+  runOnJS,
+  useAnimatedGestureHandler,
+  useSharedValue,
+} from 'react-native-reanimated';
 import CalendarBody from '../../screens/Calendar/components/CalendarBody/CalendarBody';
 import { getBeforeMonthDaysLength, getMonthDays } from '../../utils';
 import CalendarWeek from '../../screens/Calendar/components/CalendarWeek/CalendarWeek';
 import useExpandableAnimation from '../../hooks/useExpandableAnimation';
 import useCalendar from '../../hooks/useCalendar';
 import CalendarHeader from '../../screens/Calendar/components/CalendarHeader/CalendarHeader';
+import {
+  PanGestureHandler,
+  PanGestureHandlerGestureEvent,
+} from 'react-native-gesture-handler';
 
 const AnimatedView = Animated.createAnimatedComponent(View);
 
 const ExpandableList = React.memo((props: ExpandableListProps) => {
-  const { expanded, itemHeight = 0 } = props;
+  const { itemHeight = 0 } = props;
 
+  const [expanded, setExpanded] = useState(true);
   const current = new Date();
   current.setDate(1);
   const { onNextMonth, onPrevMonth, year, month } = useCalendar(current);
@@ -33,6 +42,35 @@ const ExpandableList = React.memo((props: ExpandableListProps) => {
     }
     setWeek(prev => ++prev);
   };
+  const translateX = useSharedValue(0);
+  const translateY = useSharedValue(0);
+
+  const handleFold = () => {
+    setExpanded(false);
+  };
+
+  const handleExpand = () => {
+    setExpanded(true);
+  };
+
+  const onPanGestureEvent =
+    useAnimatedGestureHandler<PanGestureHandlerGestureEvent>({
+      onStart: event => {
+        translateX.value = event.translationX;
+        translateY.value = event.translationY;
+      },
+
+      onEnd: event => {
+        if (translateY.value - event.translationY > 0) {
+          console.log('handleFold');
+          runOnJS(handleFold)();
+        }
+        if (translateY.value - event.translationY < 0) {
+          console.log('handleExpand');
+          runOnJS(handleExpand)();
+        }
+      },
+    });
 
   const onPrevWeek = async () => {
     if (week === 0) {
@@ -45,34 +83,42 @@ const ExpandableList = React.memo((props: ExpandableListProps) => {
 
   return (
     <>
+      <Button
+        title={'접기'}
+        onPress={() => {
+          setExpanded(prev => !prev);
+        }}
+      />
       <CalendarHeader
         handleNextBtn={expanded ? onNextMonth : onNextWeek}
         handlePrevBtn={expanded ? onPrevMonth : onPrevWeek}
         year={year}
         month={month}
       />
-      <AnimatedView
-        style={[
-          {
-            position: 'relative',
-            overflow: 'hidden',
-          },
-          hightAnimationStyle,
-        ]}>
+      <PanGestureHandler onGestureEvent={onPanGestureEvent}>
         <AnimatedView
           style={[
             {
-              position: 'absolute',
+              position: 'relative',
+              overflow: 'hidden',
             },
-            topAnimationStyle,
+            hightAnimationStyle,
           ]}>
-          {isWeekCalendar ? (
-            <CalendarBody days={days} />
-          ) : (
-            <CalendarWeek week={days[week]} />
-          )}
+          <AnimatedView
+            style={[
+              {
+                position: 'absolute',
+              },
+              topAnimationStyle,
+            ]}>
+            {isWeekCalendar ? (
+              <CalendarBody days={days} />
+            ) : (
+              <CalendarWeek week={days[week]} />
+            )}
+          </AnimatedView>
         </AnimatedView>
-      </AnimatedView>
+      </PanGestureHandler>
     </>
   );
 });
@@ -80,5 +126,4 @@ export default React.memo(ExpandableList);
 
 type ExpandableListProps = {
   itemHeight: number;
-  expanded: boolean;
 };
--- client/src/screens/Calendar/index.tsx
+++ client/src/screens/Calendar/index.tsx
@@ -1,24 +1,15 @@
-import React, { useState } from 'react';
-import { Button } from 'react-native';
+import React from 'react';
 
 import { CalendarContextProvider } from './components/CalendarProvider/CalendarProvider';
 import * as S from './Calender.style';
 import ExpandableList from '../../components/ExpandableList/ExpandableList';
 
 const Calendar = () => {
-  const [expand, setExpand] = useState(true);
   const current = new Date();
   return (
     <S.ScreenTemplate>
-      <Button
-        title={'접기'}
-        onPress={() => {
-          setExpand(prev => !prev);
-        }}
-      />
-
       <CalendarContextProvider initialDate={current}>
-        <ExpandableList expanded={expand} itemHeight={40} />
+        <ExpandableList itemHeight={40} />
       </CalendarContextProvider>
     </S.ScreenTemplate>
   );
